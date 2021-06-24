319e816cdf0d3bde8bb401ca17124e9cbbabde67
Aleyboy <aleyboy23@hotmail.co.uk>
2021-06-24 12:38:40 +0100

    Repo established
    A-10, Harrier, F14, and F18 liveries added

---
+++ CoreMods/aircraft/A-10/Liveries/A-10CII/TWC Squadron/description.lua
@@ -0,0 +1,21 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"TWCa",false};
+	{"A-10C_PAINT_1-b", 0 ,"TWCb",false};
+	{"A-10C_PAINT_1-c", 0 ,"TWCc",false};
+	{"A-10C_PAINT_1-d", 0 ,"TWCd",false};
+	{"A-10C_PAINT_1-e", 0 ,"TWCe",false};
+	{"A-10C_PAINT_1-f", 0 ,"TWCf",false};
+	{"A-10C_PAINT_1-g", 0 ,"TWCg",false};
+	{"A-10C_PAINT_1-h", 0 ,"TWCh",false};
+	{"A-10C_PAINT_1-i", 0 ,"TWCi",false};
+	{"A-10C_PAINT_1-j", 0 ,"TWCj",false};
+	{"A-10C_PAINT_1-k", 0 ,"TWCk",false};
+	{"A-10C_PAINT_1-L", 0 ,"TWCl",false};
+	{"A-10_Number", 0 ,"empty",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"TactNumbers-USAF-Light_black",true};
+
+}
+name = "TWC Squadron"
+countries = {"USA","UK"}
---
+++ CoreMods/aircraft/F14/Liveries/f-14b/TWC Squadron/description.lua
@@ -0,0 +1,19 @@
+livery = {
+
+
+--Diffuse  -----------------------------------
+
+{"HB_F14_EXT_01", 0 ,"HB_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"HB_F14_EXT_02",false};
+{"HB_F14_EXT_03", 0 ,"HB_F14_EXT_03",false};
+{"HB_F14_EXT_04", 0 ,"HB_F14_EXT_04",false};
+{"HB_F14_EXT_TAIL", 0 ,"HB_F14_EXT_TAIL",false};
+{"HB_F14_WING_LEFT_01", 0 ,"HB_F14_WING_LEFT",false};
+{"HB_F14_WING_RIGHT", 0 ,"HB_F14_WING_RIGHT",false};
+{"HB_F14_EXT_DROPTANK", 0 ,"HB_F14_EXT_DROPTANK",false};
+{"HB_F14_EXT_PHOENIXPYLONS", 0 ,"HB_F14_EXT_PHOENIXPYLONS",false};
+{"HB_F14_EXT_PYLONS_01", 0 ,"HB_F14_EXT_PYLONS_01",false};
+{"HB_F14_EXT_TCS", 0 ,"HB_F14_EXT_TCS",false};
+{"HB_F14_LOD1_3in1", 0 ,"HB_F14_LOD1_3in1",false};
+}
+name = "TWC Squadron"
